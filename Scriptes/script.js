7336ac31fad981bdefe0baf9c1c23942f53960e4
ishara2003 <sankalpaishara4@gmail.com>
2023-07-26 23:05:09 +0530

    About_Page

---
+++ Scriptes/script.js
@@ -0,0 +1,24 @@
+$('#git_hub').on('click',function () {
+    console.log("Entered");
+    $('#sword_sound')[0].play();
+});
+
+$('#youtube').on('click',function () {
+    console.log("Entered");
+    $('#sword_sound04')[0].play();
+});
+
+$('#instagram').on('click',function () {
+    console.log("Entered");
+    $('#sword_sound05')[0].play();
+});
+
+$('#linkedin').on('click',function () {
+    console.log("Entered");
+    $('#sword_sound02')[0].play();
+});
+
+$('#email').on('click',function () {
+    console.log("Entered");
+    $('#sword_sound03')[0].play();
+});
